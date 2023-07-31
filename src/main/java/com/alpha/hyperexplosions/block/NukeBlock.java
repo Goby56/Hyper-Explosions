9ef4cf984885254c1a2818d0764b6b1acf61fa1d
Goby56 <60710855+Goby56@users.noreply.github.com>
2023-08-01 00:40:38 +0200

    cool hitboxes

--- src/main/java/com/alpha/hyperexplosions/block/NukeBlock.java
+++ src/main/java/com/alpha/hyperexplosions/block/NukeBlock.java
@@ -3,12 +3,9 @@ package com.alpha.hyperexplosions.block;
 import com.alpha.hyperexplosions.blockentity.NukeBlockEntity;
 import com.alpha.hyperexplosions.property.ModProperties;
 import com.alpha.hyperexplosions.property.NukePart;
-import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.enums.BedPart;
 import net.minecraft.block.piston.PistonBehavior;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.TntEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -31,11 +28,20 @@ import net.minecraft.world.WorldEvents;
 import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Function;
-
 public class NukeBlock extends BlockWithEntity implements BlockEntityProvider {
     public static final EnumProperty<Direction> FACING = Properties.FACING;
     public static final EnumProperty<NukePart> PART = ModProperties.NUKE_PART;
+
+    public static final int[] FIN_HITBOX_SHAPE = new int[] {
+            5, 8,
+            11, 4,
+    };
+    public static final int[] HEAD_HITBOX_SHAPE = new int[] {
+            2, 5,
+            4, 5,
+            4, 4,
+            4, 3
+    };
 
     public NukeBlock(Settings settings) {
         super(settings);
@@ -72,7 +78,7 @@ public class NukeBlock extends BlockWithEntity implements BlockEntityProvider {
         TntEntity tntEntity = new TntEntity(world, (double)pos.getX() + 0.5, pos.getY(), (double)pos.getZ() + 0.5, igniter);
         world.spawnEntity(tntEntity);
         world.playSound(null, tntEntity.getX(), tntEntity.getY(), tntEntity.getZ(), SoundEvents.ENTITY_TNT_PRIMED, SoundCategory.BLOCKS, 1.0f, 1.0f);
-        world.emitGameEvent((Entity)igniter, GameEvent.PRIME_FUSE, pos);
+        world.emitGameEvent(igniter, GameEvent.PRIME_FUSE, pos);
     }
 
     @Nullable
@@ -162,16 +168,43 @@ public class NukeBlock extends BlockWithEntity implements BlockEntityProvider {
         return BlockRenderType.INVISIBLE;
     }
 
-//    @Override
-//    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-//        Direction direction = NukeBlock.getOppositePartDirection(state);
-//        int dx = direction.getOffsetX() * 16, dy = direction.getOffsetY() * 16, dz = direction.getOffsetZ() * 16;
-//        return Block.createCuboidShape(dx, dy, dz, 16 + dx, 16 + dy, 16 + dz);
-//    }
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        Direction facing = state.get(FACING);
+        int dx = facing.getOffsetX(), dy = facing.getOffsetY(), dz = facing.getOffsetZ();
+        VoxelShape shape = VoxelShapes.empty();
+        int[] hitbox = getHitboxShape(state.get(PART));
+        int o = 0;
+        int t;
+        int w;
+        int[] c0 = new int[3];
+        int[] c1 = new int[3];
+        for (int i = 0; i < (hitbox.length >> 1); i++) {
+            t = o + hitbox[i*2];
+            w = 8 - hitbox[i*2+1];
 
-    public static Direction getOppositePartDirection(BlockState state) {
-        Direction direction = state.get(FACING);
-        return state.get(PART) == NukePart.HEAD ? direction.getOpposite() : direction;
+            c0[0] = 16 - t;
+            c0[1] = w;
+            c0[2] = o;
+
+            c1[0] = 16 - o;
+            c1[1] = 16 - w;
+            c1[2] = t;
+
+            shape = VoxelShapes.union(shape, Block.createCuboidShape(
+                    c0[dx+1], c0[dy+1], c0[dz+1],
+                    c1[dx+1], c1[dy+1], c1[dz+1]
+            ));
+
+            o = t;
+        }
+        // TODO RETURN ENTIRE HITBOX (OTHER PART TOO)
+        return shape;
+    }
+
+    private static int[] getHitboxShape(NukePart part) {
+        if (part == NukePart.HEAD) return HEAD_HITBOX_SHAPE;
+        return FIN_HITBOX_SHAPE;
     }
 
     @Nullable
